9311e6375e0343250663a692ab9febf278a2a119
Flaya <delphine.jozeau@gmail.com>
2025-05-09 12:44:17 +0200

    console crayon OK et HTML en cours

--- crayon.js
+++ crayon.js
@@ -1,4 +1,4 @@
-/* Crée une fonction `drawMyPen(){:js}` qui retourne l'état de ton crayon en ASCII Art en fonction du temps passé en cours. Tu trouveras un exemple de crayons ci-dessous. Attention, petite subtilité : la gomme a deux étages en début d'année scolaire, mais se réduit à un seul étage à partir du mois de mars.*/
+/* Crée une fonction `drawMyPen()` qui retourne l'état de ton crayon en ASCII Art en fonction du temps passé en cours. Tu trouveras un exemple de crayons ci-dessous. Attention, petite subtilité : la gomme a deux étages en début d'année scolaire, mais se réduit à un seul étage à partir du mois de mars.*/
 
 //j'ai fait un html pour pouvoir afficher le crayon en fonction du mois selectionné dans un input select plus tard
 
@@ -15,34 +15,48 @@ const calendrierDuCrayon = {
     juin: 1,
 };
 
+//test récupération de valeur du mois OK
+const etageGrandCrayon = calendrierDuCrayon.septembre;
+console.log("test Numero Septembre : ",etageGrandCrayon)
 
+//fonction principale qui dessine tout le crayon en fonction du mois
 function drawMyPen(mois) {
-    //afficher la mine du crayon
-    mineDuCrayon();
-    //afficher les (calendrierDuCrayon.mois) etages du crayon
-    etagesDuCrayon(mois)
-    //afficher la gomme : mars avril mai juin ou de 4 à 1 : petite gomme
-    gommeDuCrayon(mois);
+    let result = "";
+    result += mineDuCrayon();
+    result += etagesDuCrayon(mois);
+    result += gommeDuCrayon(mois);
+    
+    document.getElementById("dessineLeCrayon").textContent = result;
 }
+
 
 function mineDuCrayon() {
-    //gestion de l'affichage des caractères spéciaux à trouver
-    console.log("/\\");
+    //gestion de l'affichage des caractères spéciaux
+    console.log(" /\\");
     console.log("/__\\");
+
+    let resultMine = "";
 }
+//test affichage de la mine :
+//mineDuCrayon();
+
+
 
 function etagesDuCrayon(month) {
     //récupérer le mois et sa valeur d'étage
-    let etages = calendrierDuCrayon.month
+    let etages = calendrierDuCrayon[month];
 
     //pour septembre faire 10 étages. pour le mois choisit, faire son nombre d'étage
-        for (let index = 1; index >= etages; index++) {
-            console.log("|||")
+        for (let i = 0; i < etages; i++) {
+            console.log("||||")
         }
 }
+//test etagesDuCrayon(mois)
+//etagesDuCrayon("septembre")
+
 
 function gommeDuCrayon(month) {
-    let etages = calendrierDuCrayon.month
+    let etages = calendrierDuCrayon[month];
 
     if (etages > 4) {
         console.log("|__|");
@@ -55,3 +69,8 @@ function gommeDuCrayon(month) {
         console.log("|__|");
     }
 }
+//test affichage de la gomme OK
+//gommeDuCrayon("septembre");
+
+//execution du code
+drawMyPen("novembre");
